5c1f0c9ea371bfca1a16ec53d2e1946aff40ebff
DavisOmbuki <davyomonyi@gmail.com>
2024-01-26 16:19:13 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+// Create variables
+let num1 = 10;
+let num2 = 6;
+let num3 = 15;
+let num4 = 4;
 
-// Write your code here
+// Multiply num1 and num2 to get 62
+let multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Calculate the remainder of dividing num3 by num4
+let mod = num3 % num4;
+
+// Find the highest number in a set
+let set = [10, 20, 30, 40, 50];
+let max = Math.max(...set);
+
+console.log("Multiplication result:", multiply);
+console.log("Random integer:", random);
+console.log("Remainder:", mod);
+console.log("Maximum number:", max);
